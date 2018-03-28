245a18e3c300115b41af65b99acdea2455512bdc
micahbernath <bernath.13@osu.edu>
2018-03-28 19:04:07 -0400

    Added refactoring

--- FizzBuzz.Library/FizzBuzzService.cs
+++ FizzBuzz.Library/FizzBuzzService.cs
@@ -6,22 +6,32 @@ namespace FizzBuzz.Library
         public string Print(int number)
         {
             
-            if (number % 3 == 0 && number % 5 == 0)
+            if (IsDivisibleBy3(number) && IsDivisibleBy5(number))
             {
                 return "FizzBuzz";
             }
 
-            if (number % 3 == 0)
-            {
-                return "Fizz";
-            }
-
-            if (number % 5 == 0)
+            else if (IsDivisibleBy5(number))
             {
                 return "Buzz";
             }
+
+            else if (IsDivisibleBy3(number))
+            {
+                return "Fizz";
+            }
             
             return number.ToString();
+        }
+
+        private bool IsDivisibleBy3(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        private bool IsDivisibleBy5(int number)
+        {
+            return number % 5 == 0;
         }
     }
 }
