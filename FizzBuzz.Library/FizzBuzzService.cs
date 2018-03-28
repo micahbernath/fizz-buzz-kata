39de16953b2109e5ffcab2a8fb751312d8992a03
micahbernath <bernath.13@osu.edu>
2018-03-28 18:52:52 -0400

    Added tests for fizz, buzz, and fizzbuzz

--- FizzBuzz.Library/FizzBuzzService.cs
+++ FizzBuzz.Library/FizzBuzzService.cs
@@ -2,6 +2,26 @@ namespace FizzBuzz.Library
 {
     public class FizzBuzzService
     {
-        
+
+        public string Print(int number)
+        {
+            
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            
+            return number.ToString();
+        }
     }
 }
--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -19,6 +19,28 @@ namespace FizzBuzz.Tests
         public void ShouldReturnNumber()
         {
             Assert.Equal("1", fizzBuzz.Print(1));
+            Assert.Equal("2", fizzBuzz.Print(2));
+        }
+
+        [Fact]
+
+        public void ShouldReturnFizz()
+        {
+            Assert.Equal("Fizz", fizzBuzz.Print(3));
+        }
+
+        [Fact]
+
+        public void ShouldReturnBuzz()
+        {
+            Assert.Equal("Buzz", fizzBuzz.Print(5));
+        }
+
+        [Fact]
+
+        public void ShouldReturnFizzBuzz()
+        {
+            Assert.Equal("FizzBuzz", fizzBuzz.Print(15));
         }
     }
 }
